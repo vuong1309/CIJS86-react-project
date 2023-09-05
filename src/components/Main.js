750ba66218f59557ccc17639af395802893796f1
Vuong Nguyen <nvbkhnkthh6@gmail.com>
2023-09-05 21:54:14 +0700

    final code 21h54 5-9

--- src/components/Main.js
+++ src/components/Main.js
@@ -27,7 +27,7 @@ const Main = () => {
                 <div className='absolute w-full top-[20%] p-4 md:p-8'>
                     <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
                     <div className='my-4'>
-                        <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Watch</button>
+                        <button className='border bg-gray-300 text-white border-gray-300 py-2 px-5'>Watch</button>
                         <button className='border text-white border-gray-300 py-2 px-5 ml-4'>Movie Trailer</button>
                     </div>
                     <p className='text-gray-300 text-sm italic'>Release date: {movie?.release_date}</p>
